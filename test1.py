fe17df8e10f8ec42edebc48b35da8fecf34c91aa
ArseinTeam <162746456+ArseinTeams@users.noreply.github.com>
2025-11-06 18:05:16 +0330

    teste

---
+++ test1.py
@@ -0,0 +1,17 @@
+from arsein import Messenger
+
+auth = " "
+
+key = " "
+
+phone = " "
+
+app = Messenger(auth,key)
+
+print(app.AddNumberPhone("hassan","kachal","+98{phone[1:]}"))
+print
+async def fff():
+		return "fff"
+
+
+
